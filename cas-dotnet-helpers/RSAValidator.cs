c96229555681018d7630e3eb28437de1e98d9dd1
WingZer0o <mtmulch0191@outlook.com>
2024-03-03 19:43:54 -0500

    Adding RSAValidator

---
+++ cas-dotnet-helpers/RSAValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace CASHelpers
+{
+    public static class RSAValidator
+    {
+        public static bool ValidateRsaPemKey(string pemKey)
+        {
+            try
+            {
+                var rsa = RSA.Create();
+                rsa.ImportFromPem(pemKey.ToCharArray());
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error validating RSA PEM key: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
